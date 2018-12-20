61505132d7853832b6e938e8bf1ee9e4286b6dab
Rishabh Rajput <rrishabh145@gmail.com>
2018-12-20 19:13:22 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+#Snake game python
+
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+	rows = 0
+	w = 0
+	def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+		pass
+
+	def move(self, dirnx, dirny):
+		pass
+
+	def draw(self, surface, eyes=False):
+		pass
+
+
+class snake(object):
+	"""docstring for snake"""
+	def __init__(self, color, pos):
+		pass
+	
+	def move(self):
+		pass
+
+	def reset(self, pos):
+		pass
+
+	def addCube(self):
+		pass
+
+	def draw(self,surface):
+		pass
+
+
+def drawGrid(w, rows, surface):
+	pass
+
+def redrawWindow(surface):
+	pass
+
+def randomSnack(rows,items):
+	pass
+
+def message_box(subject, content):
+	pass
+
+def main():
+	pass
+
+rows =
+w =
+h =
+
+cube.rows = rows
+cube.w = w
+
+main()
